f4f64b66fd6d36e767a70161f86ae0f2044efa77
lechibang-1512 <176822289+lechibang-1512@users.noreply.github.com>
2026-09-21 20:29:19 +0700

    fix(fighters): fully unlock Rounder Heavy Fighter and equip both Rounder wings on test Carrier

--- src/data/missions/magellan_test_protectorate/MissionDefinition.java
+++ src/data/missions/magellan_test_protectorate/MissionDefinition.java
@@ -18,7 +18,7 @@ public class MissionDefinition implements MissionDefinitionPlugin {
         api.addToFleet(FleetSide.PLAYER, "magellan_battlecruiser_Hull", FleetMemberType.SHIP, true);
         api.addToFleet(FleetSide.PLAYER, "magellan_battleship_Hull", FleetMemberType.SHIP, false);
         api.addToFleet(FleetSide.PLAYER, "magellan_carrier_Hull", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "magellan_carrier_std", FleetMemberType.SHIP, false);
+        api.addToFleet(FleetSide.PLAYER, "magellan_carrier_std", FleetMemberType.SHIP, "FSS Carrier (Rounder Wings)", false);
         api.addToFleet(FleetSide.PLAYER, "magellan_carrierconverted_Hull", FleetMemberType.SHIP, false);
         api.addToFleet(FleetSide.PLAYER, "magellan_cbtfreight_Hull", FleetMemberType.SHIP, false);
         api.addToFleet(FleetSide.PLAYER, "magellan_cruiser_Hull", FleetMemberType.SHIP, false);
