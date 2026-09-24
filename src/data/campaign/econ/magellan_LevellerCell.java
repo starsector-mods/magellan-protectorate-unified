74a9815eb0f54c740d10bd014a8bbebfa1eaaccf
lechibang-1512 <176822289+lechibang-1512@users.noreply.github.com>
2026-09-24 08:30:08 +0700

    Remove dead Java classes and unused weapon effect scripts

--- src/data/campaign/econ/magellan_LevellerCell.java
+++
@@ -1,9 +0,0 @@
-package data.campaign.econ;
-
-/**
- * Alias / subclass for market condition registry compatibility.
- */
-public class magellan_LevellerCell extends magellan_LevellerCellCondition {
-    public static final float STABILITY_PENALTY_PROTECTORATE = STABILITY_PENALTY_AUTHORITARIAN;
-    public static final float STABILITY_PENALTY_OTHER = STABILITY_PENALTY_STANDARD;
-}
